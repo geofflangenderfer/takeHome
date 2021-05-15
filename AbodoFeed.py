83a7026cb20d1311911f8c1bbc1dc88bbd568696
geoff langenderfer <geoff.langenderfer@gmail.com>
2021-05-15 16:49:54 -0500

    rearrange function order to increase readability

--- AbodoFeed.py
+++ AbodoFeed.py
@@ -6,23 +6,24 @@ class AbodoFeed:
         self.feed = minidom.parse(xml_file)
         self.properties = self.get_properties()
 
-    def get_node(self, node_name, pid):
-        out = None
-        for node in pid.childNodes:
-            # see [1] at bottom of file
-            is_node_name = type(node).__name__ == "Element" \
-                    and node.tagName == node_name
-            if is_node_name:
-                out = node
-                break
+    def get_properties(self):
+        property_ids = self.feed.getElementsByTagName('PropertyID')
+        madison_pids = self.get_madison_pids(property_ids)
+        properties = []
+        for pid in madison_pids:
+            data = self.get_desired_data(pid)
+            properties.append(data)
+
+        return properties
+
+    def get_madison_pids(self, pids):
+        out = []
+        for pid in pids:
+            city = self.get_city(pid)
+            if city == "Madison":
+                out.append(pid)
 
         return out
-
-    def get_city(self, pid):
-        address = self.get_node("Address", pid)
-        city = self.get_node("City", address)
-
-        return city.childNodes[0].nodeValue
 
     def get_desired_data(self, pid):
         property_id = self                   \
@@ -43,24 +44,24 @@ class AbodoFeed:
                 "email"       : email
         }
 
-    def get_madison_pids(self, pids):
-        out = []
-        for pid in pids:
-            city = self.get_city(pid)
-            if city == "Madison":
-                out.append(pid)
+    def get_city(self, pid):
+        address = self.get_node("Address", pid)
+        city = self.get_node("City", address)
+
+        return city.childNodes[0].nodeValue
+
+    def get_node(self, node_name, pid):
+        out = None
+        for node in pid.childNodes:
+            # see [1] at bottom of file
+            is_node_name = type(node).__name__ == "Element" \
+                    and node.tagName == node_name
+            if is_node_name:
+                out = node
+                break
 
         return out
 
-    def get_properties(self):
-        property_ids = self.feed.getElementsByTagName('PropertyID')
-        madison_pids = self.get_madison_pids(property_ids)
-        properties = []
-        for pid in madison_pids:
-            data = self.get_desired_data(pid)
-            properties.append(data)
-
-        return properties
 
 if __name__ == "__main__":
     XML = 'feed.xml'
